0dce75deded31f7afe86ec42aa7926905356901a
zhenda <1955093734@qq.com>
2024-08-01 17:08:47 +0800

    add: nginx

--- pj1/pj1/views.py
+++ pj1/pj1/views.py
@@ -1,6 +1,8 @@
+import os
 from django.http.response import HttpResponse, JsonResponse
 
 
 def home(request):
     # breakpoint()
-    return HttpResponse(f'home: {request.META.get('REMOTE_ADDR')}')
+    # return HttpResponse(f'home: {request.META.get('REMOTE_ADDR')}')
+    return HttpResponse(f'home: {os.getenv('WEB_HOST')}')
