83dcc63ab6636585ce9bf087b09c989245b79a7a
zhenda <1955093734@qq.com>
2024-08-02 15:02:58 +0800

    update: gunicorn

--- pj1/gunicorn_conf.py
+++ pj1/gunicorn_conf.py
@@ -17,8 +17,8 @@ bind = '0.0.0.0:8000'
 pidfile = './gunicorn.pid'
 
 # 设置访问日志和错误信息日志路径
-# accesslog = './logs/gunicorn_acess.log'
-# errorlog = './logs/gunicorn_error.log'
+accesslog = './logs/gunicorn_acess.log'
+errorlog = './logs/gunicorn_error.log'
 
 # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
 # debug:调试级别，记录的信息最多；
