3003e18920e98e3e84507db866026a79ed135396
zhenda <1955093734@qq.com>
2024-09-02 10:14:52 +0800

    update:2

--- pj1/pj1/views.py
+++ pj1/pj1/views.py
@@ -5,4 +5,4 @@ from django.http.response import HttpResponse, JsonResponse
 def home(request):
     # breakpoint()
     # return HttpResponse(f'home: {request.META.get('REMOTE_ADDR')}')
-    return HttpResponse(f'home: {os.getenv('WEB_HOST')} , updated')
+    return HttpResponse(f'home: {os.getenv('WEB_HOST')} , updated2')
